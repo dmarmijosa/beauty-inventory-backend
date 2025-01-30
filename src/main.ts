8306ce93d29fc265e45c48f10490a3a1acea8554
Danny Armijos <daniel2667@hotmail.es>
2025-01-30 22:46:39 +0100

    resolver cors y prueba

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:8100', // Replace with your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If you need to send cookies
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
